c496a3265ed2182e83ebc0630b0bed7aca1ba2ee
brymina18 <brymina18@yahoo.com>
2024-01-19 11:45:23 -0600

    implement loading imgs seq

--- src/components/ScrollingSection.js
+++ src/components/ScrollingSection.js
@@ -1,5 +1,39 @@
 import React from 'react';
+import { useEffect } from 'react';
 
-export default function ScrollAnimPage() {
-	return <div className="scroll-anim-section"></div>;
+
+export default function ScrollAnimPage({imgIndex}) {
+
+	const scrollImgs = [];
+
+	// Initialization
+	useEffect(()=>{
+		loadScrollImgs(scrollImgs, 30);
+	}, []);
+
+	// Updating the current img viewed in sequence based on the scrollY data passed from App component
+	useEffect(()=>{
+		console.log(imgIndex);
+	}, [imgIndex]);
+
+	function loadScrollImgs(scrollImgsRef, numOfImgs) {
+		// Load all images in a specified file
+		// Param
+		// scrollImgs (Array)
+		// -> None
+
+		for (let i = 1; i <= numOfImgs; i++) {
+			const prefixNum = i - 10 >= 0 ? i : `0${i}`; //Prefix handling
+			const imgPath = `../assets/scroll_imgs/00${prefixNum}.webp`;
+			scrollImgsRef.push(imgPath);
+		}
+		console.log(scrollImgs[0]);
+	}
+
+	return <div className="scroll-anim-section">
+		<div id='sequence-images-cont'>
+			<img src={scrollImgs[imgIndex]}>
+			</img>
+		</div>
+	</div>;
 }
